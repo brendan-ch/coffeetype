f3cb74b50019d982bce49dad47e84034c8e86650
Brendan <smallpigeon1@gmail.com>
2022-10-13 21:45:48 -0700

    use getResource to get all paths

--- src/main/java/dev/bchen/assets/Assets.java
+++ src/main/java/dev/bchen/assets/Assets.java
@@ -12,22 +12,22 @@ import javax.swing.ImageIcon;
  * scale/format them.
  */
 public class Assets {
-  public static final ImageIcon LOGO = new ImageIcon("src/main/resources/assets/coffeetype-logo.png");
-  public static final ImageIcon LOGO_WITH_TEXT = new ImageIcon("src/main/resources/assets/coffeetype-logo-with-text.png");
+  public static final ImageIcon LOGO = new ImageIcon(Assets.class.getResource("/assets/coffeetype-logo.png"));
+  public static final ImageIcon LOGO_WITH_TEXT = new ImageIcon(Assets.class.getResource("/assets/coffeetype-logo-with-text.png"));
 
-  public static final ImageIcon RESTART_TEST = new ImageIcon("src/main/resources/assets/restart-test.png");
-  public static final ImageIcon NEXT_TEST = new ImageIcon("src/main/resources/assets/next-test.png");
-  public static final ImageIcon SETTINGS = new ImageIcon("src/main/resources/assets/settings.png");
-  public static final ImageIcon HISTORY = new ImageIcon("src/main/resources/assets/history.png");
-  public static final ImageIcon BACK = new ImageIcon("src/main/resources/assets/back.png");
-  public static final ImageIcon INVITE = new ImageIcon("src/main/resources/assets/invite.png");
-  public static final ImageIcon EXIT = new ImageIcon("src/main/resources/assets/exit.png");
+  public static final ImageIcon RESTART_TEST = new ImageIcon(Assets.class.getResource("/assets/restart-test.png"));
+  public static final ImageIcon NEXT_TEST = new ImageIcon(Assets.class.getResource("/assets/next-test.png"));
+  public static final ImageIcon SETTINGS = new ImageIcon(Assets.class.getResource("/assets/settings.png"));
+  public static final ImageIcon HISTORY = new ImageIcon(Assets.class.getResource("/assets/history.png"));
+  public static final ImageIcon BACK = new ImageIcon(Assets.class.getResource("/assets/back.png"));
+  public static final ImageIcon INVITE = new ImageIcon(Assets.class.getResource("/assets/invite.png"));
+  public static final ImageIcon EXIT = new ImageIcon(Assets.class.getResource("/assets/exit.png"));
   
-  public static final ImageIcon TOGGLE_SWITCH_ON = new ImageIcon("src/main/resources/assets/toggle-switch-on.png");
-  public static final ImageIcon TOGGLE_SWITCH_OFF = new ImageIcon("src/main/resources/assets/toggle-switch-off.png");
+  public static final ImageIcon TOGGLE_SWITCH_ON = new ImageIcon(Assets.class.getResource("/assets/toggle-switch-on.png"));
+  public static final ImageIcon TOGGLE_SWITCH_OFF = new ImageIcon(Assets.class.getResource("/assets/toggle-switch-off.png"));
 
-  public static final Font DISPLAY_FONT = new Font("src/main/resources/assets/display-font.ttf", Font.TRUETYPE_FONT, 15);
-  public static final Font DISPLAY_FONT_BIG = new Font("src/main/resources/assets/display-font.ttf", Font.TRUETYPE_FONT, 35);
+  public static final Font DISPLAY_FONT = new Font("/assets/display-font.ttf", Font.TRUETYPE_FONT, 15);
+  public static final Font DISPLAY_FONT_BIG = new Font("/assets/display-font.ttf", Font.TRUETYPE_FONT, 35);
   public static final Font TYPING_FONT = new Font("Monospaced", Font.TRUETYPE_FONT, 15);
   public static final Font TYPING_FONT_BIG = new Font("Monospaced", Font.TRUETYPE_FONT, 35);
 
